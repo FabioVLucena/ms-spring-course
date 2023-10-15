757541ea1ba9d91d99e03dcb5d49e00188eb4514
aeon <fabio-trab@outlook.com>
2023-10-15 15:58:30 -0400

    add starter sql script

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO worker (name, daily_Income) VALUES ('Athos', 200.0);
+INSERT INTO worker (name, daily_Income) VALUES ('Porthos', 300.0);
+INSERT INTO worker (name, daily_Income) VALUES ('Aramis', 250.0);
