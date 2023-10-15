c39594df8311c210991a289fce200a9b0173d60e
aeon <fabio-trab@outlook.com>
2023-10-15 16:54:24 -0400

    add rest template payment

--- hr-payroll/src/main/java/com/aeon/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/aeon/hrpayroll/service/PaymentService.java
@@ -1,14 +1,36 @@
 package com.aeon.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.aeon.hrpayroll.entity.Payment;
+import com.aeon.hrpayroll.entity.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String HR_WORKER_HOST;
+	
+	private RestTemplate restTemplate;
+	
+	public PaymentService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Athos", 200.0, days);
+		Map<String, String> uriVariable = new HashMap<String, String>();
+		uriVariable.put("id", workerId.toString());
+		
+		String url = HR_WORKER_HOST + "/workers/{id}";
+		
+		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariable); 
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
