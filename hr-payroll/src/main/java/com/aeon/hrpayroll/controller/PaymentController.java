9dd7d60587037f36ea688f4a5ba3d321e8a5c57d
aeon <fabio-trab@outlook.com>
2023-10-16 22:30:16 -0400

    hystrix error handling

--- hr-payroll/src/main/java/com/aeon/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/aeon/hrpayroll/controller/PaymentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aeon.hrpayroll.entity.Payment;
 import com.aeon.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/api/v1/payments")
@@ -20,11 +21,18 @@ public class PaymentController {
 		this.paymentService = paymentService;
 	}
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = paymentService.getPayment(workerId, days);
 		
 		return ResponseEntity.status(HttpStatus.OK).body(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Faker", 700.0, days);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(payment);		
 	}
 	
 }
