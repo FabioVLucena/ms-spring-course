04a7f79c1733ccb98ad63bf542779b71625b8526
aeon <fabio-trab@outlook.com>
2023-10-30 22:27:05 -0400

    implements login JWT token to hr oauth

--- hr-oauth/src/main/java/com/aeon/hroauth/entity/User.java
+++ hr-oauth/src/main/java/com/aeon/hroauth/entity/User.java
@@ -1,9 +1,17 @@
 package com.aeon.hroauth.entity;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	
@@ -85,6 +93,37 @@ public class User {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
 		return true;
 	}
 }
